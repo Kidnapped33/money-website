d348309e00244db897aadd0239df4efecd29f33f
Ashine <404770266@qq.com>
2020-10-21 15:57:35 +0800

    deploy

---
+++ precache-manifest.01e8a7bab04f8fd93f56aed5ba17d3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb5f3d55174c5dbcd9a",
+    "url": "/money-website/css/app.ddc4f401.css"
+  },
+  {
+    "revision": "0901908fc53e21c5309fd355cdfd1fb3",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "ebb5f3d55174c5dbcd9a",
+    "url": "/money-website/js/app.6a4c7c53.js"
+  },
+  {
+    "revision": "7c9122761b6d1e9e6c5f",
+    "url": "/money-website/js/chunk-vendors.733f49aa.js"
+  },
+  {
+    "revision": "f32dd2cc1c04680990eb73918d11e7cd",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
